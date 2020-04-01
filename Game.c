58c76c678411969fd5424c5167ce498abe96baa7
heenamkim <znzltiq111@naver.com>
2020-04-01 20:40:45 +0900

    project

--- Game.c
+++ Game.c
@@ -9,24 +9,24 @@
 #define ABOUT           5
 #define EXIT            0
 #define NUM_OF_SIMULATION 10000
-
+//difine 이용해서 치환
 int displayMainMenu();
 void diceSimulation();
 void gameShow();
 void highLowGame();
 void wordGame();
 void about();
-
+// 사용할 함수 만들어 주기
 int main()
 {
     int menu=1;
     while(menu!=0) {
 
         menu = displayMainMenu();
-        switch(menu) {
+        switch(menu) { //switch문은  if문보다 빠름 if == 과 같은 의미
             case DICE_SIMULATION:
                 diceSimulation();
-                break;
+                break; // switch case문을 쓸때는 break를 해줘야 함 아니면 입력받은 것 아래에 있는 것들도 실행
             case GAME_SHOW:
                 gameShow();
                 break;
@@ -47,26 +47,26 @@ int main()
     } //wile
 } // main
 
-int displayMainMenu()
+int displayMainMenu() //메인 함수
 {
-    int menu;
+    int menu; //선택 할수 있는 메뉴를 보여줌
 
-    printf("===================\n");
-    printf("GAME MENU\n");
-    printf("___________________\n");
-    printf("1.Dice Simulation\n");
-    printf("2.Game show\n");
-    printf("3.High Low Game\n");
-    printf("4.Word Game\n");
-    printf("5.About\n");
-    printf("___________________\n");
-    printf("0.Exit\n");
-    printf("===================\n");
-    printf("Game Menu Number>>");
-    scanf("%d",&menu);
-    printf("===================\n");
+    printf("  ===================\n");
+    printf("  GAME MENU\n");
+    printf("  ___________________\n");
+    printf("  1.Dice Simulation\n");
+    printf("  2.Game show\n");
+    printf("  3.High Low Game\n");
+    printf("  4.Word Game\n");
+    printf("  5.About\n");
+    printf("  ___________________\n");
+    printf("  0.Exit\n");
+    printf("  ===================\n");
+    printf("  Game Menu Number>>");
+    scanf("  %d",&menu); //실행할 메뉴 선택
+    printf("  ===================\n");
 
-    return menu;
+    return menu; //메뉴값 리턴 받기
 }
 
 void diceSimulation() {
@@ -84,10 +84,10 @@ void diceSimulation() {
             count++;
         }
     }
-    printf("avg = %.2lf\n", (double)count/NUM_OF_SIMULATION);
+    printf("  avg = %.2lf\n", (double)count/NUM_OF_SIMULATION);
 }
 
-void gameShow() {
+void gameShow() { //확률을 보여주는 함수
     int total=0;
     int count=0;
     int my_door, prize_door, another_door, new_door;
@@ -99,7 +99,7 @@ void gameShow() {
         if (prize_door == my_door)
             count++;
     }
-    printf("%.2lf%%\n",(double)count/NUM_OF_SIMULATION*100);
+    printf("  %.2lf%%\n",(double)count/NUM_OF_SIMULATION*100); //변수 하나만 실수로 바꿔 줘도 실수로 출력가능
     count = 0;
     for (int i = 0; i < NUM_OF_SIMULATION; i++){
         prize_door = rand() % 3;
@@ -115,36 +115,36 @@ void gameShow() {
         if (prize_door == new_door)
             count++;
     }
-    printf("%.2lf%%\n", (double)count/NUM_OF_SIMULATION*100);
+    printf("  %.2lf%%\n", (double)count/NUM_OF_SIMULATION*100);
 }
 
 void highLowGame() {
     int ran,num;
     srand(time(0));
     ran = rand() % 100;
-    printf("High Low Game Start!\n");
-    printf("___________________\n");
+    printf("  High Low Game Start!\n");
+    printf("  ___________________\n");
         while (1){
-            printf("Num >>");
+            printf("  Num >>");
             scanf("%d",&num);
             if (ran > num)
-                printf("High!\n");
+                printf("  High!\n");
             else if (ran < num)
-                printf("Low..\n");
+                printf("  Low..\n");
             else if (ran == num){
-                printf("That's Right!\n");
+                printf("  That's Right!\n");
                 break;
             }
         }
 }
 
 void wordGame() {
-    printf("4.Word Game\n");
+    printf("  4.Word Game\n");
 }
 
 void about() {
-    printf("Sujong university\n");
-    printf("somputher Engineering\n");
-    printf("Created by heenam.k on 2020\n");
-    printf("copyright(c) 2020y heenam.k\n");
+    printf("  Sujong university\n");
+    printf("  somputher Engineering\n");
+    printf("  Created by heenam.k on 2020\n");
+    printf("  copyright(c) 2020y heenam.k\n");
 }
