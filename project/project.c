2e2b4a0e3fd926d7e32b618d53c320fc39498533
heenamkim <znzltiq111@naver.com>
2020-05-11 22:34:25 +0900

    project

---
+++ project/project.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Bread{
+    char name[20]; // 이름
+    int price; //가격
+    int quantity; // 수량
+    int expiation_date;// 유통기한
+
+};
+
+int main()
+{
+    return 0;
+}
