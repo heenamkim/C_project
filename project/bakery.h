cc65beef90cb63a935741e69d28148ff5e0209b8
heenamkim <znzltiq111@naver.com>
2020-05-18 19:14:54 +0900

    project

---
+++ project/bakery.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+#define INSERT   1
+#define DELETE   2
+#define UPDATE   3
+#define RETRIEVE 4
+#define PRINT    5
+#define EXIT     0
+
+struct Bread{
+    char name[20]; // 이름
+    int price; //가격
+    int quantity; // 수량
+    int expiation_date;// 유통기한
+};
+
+int displayMainMenu();
+void Insert();
+void Delete();
+void Update();
+void Retrieve();
+void Print();
--- project/project.c
+++ project/project.c
@@ -1,15 +1,71 @@
-#include <stdio.h>
-#include <string.h>
-
-struct Bread{
-    char name[20]; // 이름
-    int price; //가격
-    int quantity; // 수량
-    int expiation_date;// 유통기한
-
-};
+#include "bakery.h"
 
 int main()
 {
+    int menu = 1;
+
+    while(menu != 0){
+
+        menu = displayMainMenu();
+        switch (menu) {
+            case INSERT:
+                Insert();
+                break;
+            case DELETE:
+                Delete();
+                break;
+            case UPDATE:
+                Update();
+                break;
+            case RETRIEVE:
+                Retrieve();
+                break;
+            case PRINT:
+                Print();
+                break;
+            case EXIT:
+                break;
+            }
+    }
     return 0;
+}
+
+int displayMainMenu()
+{
+    int menu;
+
+    printf("\n  ==================\n");
+    printf("  Management Profram\n");
+    printf("  1. Insert\n");
+    printf("  2. Delete\n");
+    printf("  3. Update\n");
+    printf("  4. Retrieve\n");
+    printf("  5. Print\n");
+    printf("  0. Exit\n");
+    printf("  ___________________\n");
+    printf("  INPUT >> ");
+    scanf("%d", &menu);
+
+    return menu;
+}
+
+void Insert()
+{
+    printf("insert");
+}
+void Delete()
+{
+    printf("delete");
+}
+void Update()
+{
+    printf("update");
+}
+void Retrieve()
+{
+    printf("retrieve");
+}
+void Print()
+{
+    printf("print");
 }
